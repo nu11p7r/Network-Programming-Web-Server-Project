088499314aa5f4d939cbb75ab52723840dadaa55
nu11p7r <pmis0906@protonmail.com>
2025-12-12 00:18:40 +0900

    New

--- Server/DatabaseManager.h
+++ Server/DatabaseManager.h
@@ -4,7 +4,7 @@
 
 // 데이터베이스 연결 인포 현재 PC에서 사용중인 userName과 password로 설정 필요
 // "host=localhost port=5432 dbname=ott_db user=user1234 password=pass1234"
-#define DB_CONNINFO "host=localhost port=5432 dbname=ott_db user=postgres password=root"
+#define DB_CONNINFO "host=localhost port=5432 dbname=ott_db user=postgres password=pass1234"
 
 void DbTestConnection();
 int DbRegister(const char *szUsername, const char *szPassword);
